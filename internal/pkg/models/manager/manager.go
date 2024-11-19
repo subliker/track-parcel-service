27fa4cc70ce9bc735bcdc4d188c6d990544e50ce
subliker <subliker@SUBLIKER>
2024-11-20 02:36:23 +0300

    feat: user and manager models

--- internal/pkg/models/manager/manager.go
+++ internal/pkg/models/manager/manager.go
@@ -1,9 +1,11 @@
-package user
+package manager
 
 import "github.com/subliker/track-parcel-service/internal/pkg/models/telegram"
 
-type User struct {
+type Manager struct {
 	TelegramId  telegram.ID
 	FullName    string
+	Email       string
 	PhoneNumber *string
+	Company     *string
 }
